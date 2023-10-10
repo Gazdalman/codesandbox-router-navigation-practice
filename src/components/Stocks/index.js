cc438fe9fa426f22148f4e34463362beab9e2913
Toney Winston <137526632+Gazdalman@users.noreply.github.com>
2023-10-10 15:25:55 -0500

    Initial commit

---
+++ src/components/Stocks/index.js
@@ -0,0 +1,21 @@
+import { Redirect, useHistory } from "react-router-dom";
+function Stocks() {
+  const history = useHistory();
+  console.log(history);
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+    history.push("/");
+  };
+  const loggedIn = true;
+  if (!loggedIn) {
+    return <Redirect to="/not-logged-in" />;
+  }
+  return (
+    <div className="comp orange">
+      <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
+    </div>
+  );
+}
+
+export default Stocks;
